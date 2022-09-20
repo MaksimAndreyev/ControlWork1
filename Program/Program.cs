930784ca627938332812b91f41966854b0ec4757
maximandreev <maxands@yandex.ru>
2022-09-20 17:59:38 +0300

    Program created

---
+++ Program/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите массив:");
+string[] input = Console.ReadLine().Split();
+int count = 0;
+for (int i=0; i<input.Length; i++)
+{
+    if (input[i].Length <= 3)
+    {
+        count++;
+    }
+}
+string[] array = new string[count];
+int j = 0;
+for (int i=0; i<input.Length; i++)
+{
+    if (input[i].Length <= 3)
+    {
+        array[j] = input[i];
+        j++;
+    }
+}
+foreach (string element in array)
+{
+    Console.Write(element + " ");
+}
